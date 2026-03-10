d558266ab9b36c46e1834a7dc13a3a753d623511
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-03-09 20:25:33 -0700

    chore: add Netlify React Router Vite plugin

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vitest" />
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -11,6 +12,7 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
+    netlifyReactRouter({ edge: true }),
   ],
   esbuild: {
     supported: {
