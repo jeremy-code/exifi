2802ac19333a13fbbfff04196e11d3a927e15bc4
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-04-09 23:23:44 -0700

    feat: enhance ValueCell to support EXIF datetime formatting and conversion

--- apps/client/src/routes/(app)/editor/-components/ValueCell.tsx
+++ apps/client/src/routes/(app)/editor/-components/ValueCell.tsx
@@ -7,16 +7,60 @@ type ValueCellProps = CellContext<
   ExifEntryObject["formattedValue"]
 >;
 
+const EXIF_TIMESTAMP_REGEX =
+  /^(?<year>\d{4}):(?<month>\d{2}):(?<day>\d{2}) (?<hour>\d{2}):(?<minute>\d{2}):(?<second>\d{2})$/;
+
+const convertExifTimestampToIsoTimestamp = (exifTimestamp: string) => {
+  return exifTimestamp.replace(
+    EXIF_TIMESTAMP_REGEX,
+    "$<year>-$<month>-$<day>T$<hour>:$<minute>:$<second>",
+  );
+};
+
+const formatDateAsExifTimestamp = (date: Date) => {
+  const year = date.getFullYear().toString().padStart(4, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+
+  return `${year}:${month}:${day} ${hours}:${minutes}:${seconds}`;
+};
+
+// https://github.com/libexif/libexif/blob/b9b7f3c08c1b6812ad3b9d62227ad9527ab9385a/libexif/exif-entry.c#L1718
+const DATETIME_TAGS = [
+  "DATE_TIME",
+  "DATE_TIME_ORIGINAL",
+  "DATE_TIME_DIGITIZED",
+];
+
 const ValueCell = ({ getValue, row, table }: ValueCellProps) => {
   const value = getValue() ?? "";
+  const isAscii = row.original.format === "ASCII";
+  const isDateTime = DATETIME_TAGS.includes(row.original.tag);
 
   return (
     <input
       className="border border-transparent focus:border-border focus:bg-background focus:outline-none"
-      disabled={row.original.format !== "ASCII"}
-      value={value}
+      disabled={!isAscii}
+      type={isDateTime ? "datetime-local" : "text"}
+      value={!isDateTime ? value : convertExifTimestampToIsoTimestamp(value)}
       onChange={(e) => {
-        table.options.meta?.updateExifEntry(row.original, e.target.value);
+        if (!isDateTime) {
+          table.options.meta?.updateExifEntry(row.original, e.target.value);
+        } else {
+          console.log(e.target.value);
+          console.log(
+            formatDateAsExifTimestamp(new Date(Date.parse(e.target.value))),
+          );
+          if (e.target.value !== "") {
+            table.options.meta?.updateExifEntry(
+              row.original,
+              formatDateAsExifTimestamp(new Date(Date.parse(e.target.value))),
+            );
+          }
+        }
       }}
     />
   );
