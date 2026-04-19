14aea91eaead147cf09e79724da572329fb3fc1a
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-04-18 21:12:38 -0700

    refactor: improve segment handling in splitJpegIntoSegments

--- packages/write-exif-data/src/jpeg/splitJpegIntoSegments.ts
+++ packages/write-exif-data/src/jpeg/splitJpegIntoSegments.ts
@@ -1,9 +1,12 @@
 import { JpegMarker } from "../constants";
-import { assertEndsWithEOI } from "./assertEndsWithEOI";
 import { assertMarker } from "./assertMarker";
 
 const splitJpegIntoSegments = (jpegImage: Uint8Array): Uint8Array[] => {
-  const dataView = new DataView(jpegImage.buffer);
+  const dataView = new DataView(
+    jpegImage.buffer,
+    jpegImage.byteOffset,
+    jpegImage.byteLength,
+  );
 
   const segments: Uint8Array[] = [];
   let byteOffset = 0;
@@ -12,34 +15,42 @@ const splitJpegIntoSegments = (jpegImage: Uint8Array): Uint8Array[] => {
     assertMarker(jpegImage, byteOffset);
     const markerSecondByte = dataView.getUint8(byteOffset + 1);
 
-    if (
-      markerSecondByte === JpegMarker.SOI ||
-      markerSecondByte === JpegMarker.EOI
-    ) {
-      segments.push(jpegImage.subarray(byteOffset, byteOffset + 2));
-      byteOffset += 2;
-      continue;
-    } else if (
-      // RST0–RST7
-      markerSecondByte >= JpegMarker.RST0 &&
-      markerSecondByte <= JpegMarker.RST7
-    ) {
-      segments.push(jpegImage.subarray(byteOffset, byteOffset + 2));
-      byteOffset += 2;
-      continue;
-    } else if (markerSecondByte === JpegMarker.SOS) {
-      // Start of Scan (scan data until next marker)
-      assertEndsWithEOI(jpegImage);
-      segments.push(jpegImage.subarray(byteOffset, jpegImage.length - 2));
-      segments.push(jpegImage.subarray(jpegImage.length - 2)); // Append EOI
-      break;
+    switch (markerSecondByte) {
+      case JpegMarker.SOI:
+      case JpegMarker.EOI:
+        segments.push(jpegImage.subarray(byteOffset, byteOffset + 2));
+        byteOffset += 2;
+        break;
+      case JpegMarker.RST0:
+      case JpegMarker.RST1:
+      case JpegMarker.RST2:
+      case JpegMarker.RST3:
+      case JpegMarker.RST4:
+      case JpegMarker.RST5:
+      case JpegMarker.RST6:
+      case JpegMarker.RST7:
+        segments.push(jpegImage.subarray(byteOffset, byteOffset + 2));
+        byteOffset += 2;
+        break;
+      case JpegMarker.SOS:
+        segments.push(jpegImage.subarray(byteOffset, jpegImage.length - 2));
+        byteOffset = jpegImage.length - 2;
+        break;
+      default: {
+        // Big Endian https://github.com/corkami/formats/blob/master/image/jpeg.md#structure
+        const segmentLength = dataView.getUint16(byteOffset + 2, false);
+        // Length includes itself, so must be at least 2
+        if (segmentLength < 2) {
+          throw new Error("Invalid segment: segment length was less than two");
+        }
+
+        const endOfSegmentIndex = byteOffset + 2 + segmentLength;
+
+        segments.push(jpegImage.subarray(byteOffset, endOfSegmentIndex));
+        byteOffset = endOfSegmentIndex;
+        break;
+      }
     }
-
-    const chunkLength = dataView.getUint16(byteOffset + 2, false);
-    const endOfChunkIndex = byteOffset + 2 + chunkLength;
-
-    segments.push(jpegImage.subarray(byteOffset, endOfChunkIndex));
-    byteOffset = endOfChunkIndex;
   }
 
   return segments;
