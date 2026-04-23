82d6a1846fb4350aa3dd10417e77b7112786b646
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-04-22 21:13:00 -0700

    refactor: simplify useBreakpoint hook by utilizing useSyncExternalStore

--- apps/client/src/hooks/useBreakpoint.tsx
+++ apps/client/src/hooks/useBreakpoint.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useEffectEvent, useState } from "react";
+import { useSyncExternalStore } from "react";
 
 type Breakpoint = "sm" | "md" | "lg" | "xl" | "2xl";
 
@@ -10,23 +10,19 @@ const useBreakpoint = (breakpoint: BreakpointWithUtility) => {
     .getPropertyValue(
       `--breakpoint-${breakpoint.startsWith("max-") ? breakpoint.slice("max-".length) : breakpoint}`,
     );
-  const [matches, setMatches] = useState(false);
-  const handleChange = useEffectEvent((event: MediaQueryListEvent) =>
-    setMatches(event.matches),
+  const mediaQueryList =
+    breakpoint.startsWith("max-") ?
+      window.matchMedia(`(width < ${breakpointValue})`)
+    : window.matchMedia(`(width >= ${breakpointValue})`);
+
+  const matches = useSyncExternalStore(
+    (onStoreChange) => {
+      mediaQueryList.addEventListener("change", onStoreChange);
+
+      return () => mediaQueryList.removeEventListener("change", onStoreChange);
+    },
+    () => mediaQueryList.matches,
   );
-
-  useEffect(() => {
-    const mediaQueryList =
-      breakpoint.startsWith("max-") ?
-        window.matchMedia(`(width < ${breakpointValue})`)
-      : window.matchMedia(`(width >= ${breakpointValue})`);
-
-    mediaQueryList.addEventListener("change", handleChange);
-
-    return () => {
-      mediaQueryList.removeEventListener("change", handleChange);
-    };
-  });
 
   return matches;
 };
