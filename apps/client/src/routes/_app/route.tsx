fab2fc496786fb160314568b2944b0049800c723
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-08-27 20:37:44 -0700

    feat(client): allow url search param to viewer/editor pages
    - Closes #24

--- apps/client/src/routes/_app/route.tsx
+++ apps/client/src/routes/_app/route.tsx
@@ -1,8 +1,38 @@
+import { useEffect } from "react";
+
 import { Outlet, createFileRoute } from "@tanstack/react-router";
+import * as z from "zod";
+import { useShallow } from "zustand/react/shallow";
 
 import { FileTabs } from "#components/tabs/FileTabs";
+import { useFileTabsStore } from "#stores/fileTabsStore";
+import { getFileFromResponse } from "#utils/getFileFromResponse";
+
+const appLayoutSearchSchema = z.object({
+  url: z.url().optional().catch(undefined),
+});
 
 const AppLayoutComponent = () => {
+  const { url } = Route.useSearch();
+  const { activeTabId, updateTab } = useFileTabsStore(
+    useShallow((state) => ({
+      activeTabId: state.activeTabId,
+      updateTab: state.updateTab,
+    })),
+  );
+
+  useEffect(() => {
+    if (url !== undefined) {
+      const abortController = new AbortController();
+
+      void fetch(url, { signal: abortController.signal })
+        .then((response) => getFileFromResponse(response))
+        .then((file) => updateTab({ id: activeTabId, file }));
+
+      return () => abortController.abort();
+    }
+  }, [activeTabId, updateTab, url]);
+
   return (
     <FileTabs>
       <Outlet />
@@ -11,6 +41,7 @@ const AppLayoutComponent = () => {
 };
 
 const Route = createFileRoute("/_app")({
+  validateSearch: appLayoutSearchSchema,
   component: AppLayoutComponent,
   ssr: false,
 });
