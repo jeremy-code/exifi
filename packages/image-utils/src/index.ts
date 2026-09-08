244383427c66a258b2c8c8773d940f13bf2a7b92
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-09-07 19:43:35 -0700

    feat(image-utils): add libwebp Exif support

--- packages/image-utils/src/index.ts
+++ packages/image-utils/src/index.ts
@@ -6,6 +6,12 @@ import ImageUtilsModule from "@exifi/image-utils/imageUtils";
 // in practice
 const ImageUtils = await ImageUtilsModule();
 
-// oxlint-disable-next-line typescript/unbound-method -- Methods do not rely on this
-export const { jpeg_set_exif_data, png_get_exif_data, png_set_exif_data } =
-  ImageUtils;
+/* oxlint-disable typescript/unbound-method -- Methods do not rely on this */
+export const {
+  jpeg_set_exif_data,
+  png_get_exif_data,
+  png_set_exif_data,
+  webp_get_exif_data,
+  webp_set_exif_data,
+} = ImageUtils;
+/* oxlint-enable typescript/unbound-method */
