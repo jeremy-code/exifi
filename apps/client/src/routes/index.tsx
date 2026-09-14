d117c33ec8fd81c0177ce8fb49c10801c94f15e1
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-09-13 19:54:32 -0700

    fix(client): fix svg prop naming scheme

--- apps/client/src/routes/index.tsx
+++ apps/client/src/routes/index.tsx
@@ -45,9 +45,9 @@ const FEATURES = [
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className="size-4"
       >
         <Lock />
