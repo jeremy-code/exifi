45b329d0767b421c413097b3c9ac56cba4a7e61f
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-09-13 17:01:38 -0700

    feat(client): add notice that write support for AVIF/HEIC/HEIF is not avaliable
    - Closes #51

--- apps/client/src/features/exif-editor/ExifEditor.tsx
+++ apps/client/src/features/exif-editor/ExifEditor.tsx
@@ -3,6 +3,8 @@ import { Suspense, type ComponentPropsWithRef } from "react";
 import { cn } from "tailwind-variants";
 
 import { FileInformation } from "#components/file/FileInformation";
+import { Callout, CalloutText } from "@exifi/ui/components/Callout";
+import { Link } from "@exifi/ui/components/Link";
 import { Skeleton } from "@exifi/ui/components/Skeleton";
 
 import { ExifEditorContent } from "./ExifEditorContent";
@@ -14,6 +16,27 @@ type ExifEditorProps = {
 const ExifEditor = ({ file, className, ...props }: ExifEditorProps) => {
   return (
     <div className={cn("flex flex-col gap-4", className)} {...props}>
+      {file.type === "image/heif" ||
+      file.type === "image/heic" ||
+      file.type === "image/avif" ? (
+        <Callout variant="warning" className="mb-2 w-full">
+          <CalloutText>
+            <span>
+              {
+                "Are you trying to edit a HEIF/HEIC/AVIF image? For now, exifi does not support updating Exif data for those images. For more information, see "
+              }
+              <Link
+                color="blue"
+                href="https://github.com/jeremy-code/exifi/issues/13"
+              >
+                jeremy-code/exifi#13
+              </Link>
+              {"."}
+            </span>
+          </CalloutText>
+        </Callout>
+      ) : null}
+
       <FileInformation file={file} />
       <Suspense fallback={<Skeleton className="h-50 w-full" />}>
         <ExifEditorContent file={file} />
