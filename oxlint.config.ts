8a6d5c9ffc716d6a384597f79a7410e723639fb8
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-09-13 21:23:19 -0700

    chore: enable reportUnusedDisableDirectives in Oxlint

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -5,6 +5,7 @@ import baseConfig from "@exifi/oxlint-config";
 export default defineConfig({
   extends: [baseConfig],
   options: {
+    reportUnusedDisableDirectives: "warn",
     typeAware: true,
     typeCheck: true,
   },
