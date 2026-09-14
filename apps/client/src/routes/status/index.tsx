c41f68676be660430db028b53dae87e029cd0f16
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-09-13 19:34:51 -0700

    chore(client): update Status page
    - Add heading
    - Link to license.md
    - Put DataList inside a Card
    - Put timestamp inside time eleent

--- apps/client/src/routes/status/index.tsx
+++ apps/client/src/routes/status/index.tsx
@@ -1,51 +1,73 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useDateFormatter } from "react-aria/useDateFormatter";
 
+import { getBaseUrl } from "#utils/getBaseUrl";
 import { seo } from "#utils/seo";
+import { Card } from "@exifi/ui/components/Card";
 import {
   DataList,
   DataListItem,
   DataListItemLabel,
   DataListItemValue,
 } from "@exifi/ui/components/DataList";
+import { Heading } from "@exifi/ui/components/Heading";
 import { Link } from "@exifi/ui/components/Link";
 
 const StatusComponent = () => {
   const dateFormatter = useDateFormatter();
+  const buildTimestampInstant =
+    Temporal.Instant.fromEpochMilliseconds(__BUILD_TIMESTAMP__);
 
   return (
-    <div className="container py-8">
-      <DataList>
-        <DataListItem className="max-sm:flex-col!">
-          <DataListItemLabel className="min-w-40">Build time</DataListItemLabel>
-          <DataListItemValue>
-            {dateFormatter.format(
-              Temporal.Instant.fromEpochMilliseconds(__BUILD_TIMESTAMP__),
-            )}
-          </DataListItemValue>
-        </DataListItem>
-        <DataListItem className="max-sm:flex-col!">
-          <DataListItemLabel className="min-w-40">
-            libexif-wasm version
-          </DataListItemLabel>
-          <DataListItemValue>{__LIBEXIF_WASM_VERSION__}</DataListItemValue>
-        </DataListItem>
-        <DataListItem className="max-sm:flex-col!">
-          <DataListItemLabel className="min-w-40">Commit</DataListItemLabel>
-          <DataListItemValue>
-            {import.meta.env.COMMIT_REF !== undefined ? (
-              <Link
-                isExternal
-                href={`https://www.github.com/jeremy-code/exifi/commit/${import.meta.env.COMMIT_REF}`}
-              >
-                {import.meta.env.COMMIT_REF}
-              </Link>
-            ) : (
-              "Unknown commit"
-            )}
-          </DataListItemValue>
-        </DataListItem>
-      </DataList>
+    <div className="container flex flex-col gap-8 py-8">
+      <div>
+        <Heading level={1} size="2xl" className="mb-4">
+          Status
+        </Heading>
+        <p className="text-fg-muted">
+          Licenses of bundled JavaScript dependencies can be found at{" "}
+          <Link color="link" href="/license.md">
+            {`${getBaseUrl()}/license.md`}
+          </Link>
+          {"."}
+        </p>
+      </div>
+      <Card className="p-6">
+        <DataList>
+          <DataListItem className="max-sm:flex-col!">
+            <DataListItemLabel className="min-w-40">
+              Build time
+            </DataListItemLabel>
+            <DataListItemValue>
+              <time dateTime={buildTimestampInstant.toString()}>
+                {dateFormatter.format(buildTimestampInstant)}
+              </time>
+            </DataListItemValue>
+          </DataListItem>
+          <DataListItem className="max-sm:flex-col!">
+            <DataListItemLabel className="min-w-40">
+              libexif-wasm version
+            </DataListItemLabel>
+            <DataListItemValue>{__LIBEXIF_WASM_VERSION__}</DataListItemValue>
+          </DataListItem>
+          <DataListItem className="max-sm:flex-col!">
+            <DataListItemLabel className="min-w-40">Commit</DataListItemLabel>
+            <DataListItemValue>
+              {import.meta.env.COMMIT_REF !== undefined &&
+              import.meta.env.COMMIT_REF !== "" ? (
+                <Link
+                  isExternal
+                  href={`https://www.github.com/jeremy-code/exifi/commit/${import.meta.env.COMMIT_REF}`}
+                >
+                  {import.meta.env.COMMIT_REF}
+                </Link>
+              ) : (
+                "Unknown commit"
+              )}
+            </DataListItemValue>
+          </DataListItem>
+        </DataList>
+      </Card>
     </div>
   );
 };
